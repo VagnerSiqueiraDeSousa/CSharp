1cf5f8a38b66409a34ac373b9e72448b475d9363
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 18:42:29 -0300

    Add files via upload
    atribuicoes

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Oi Vagner, estamos Executando o projeto 6");
+
+            int idade = 38;
+            int idadeVagner = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeVagner);
+
+            
+
+
+
+            Console.WriteLine("Presseione a enter para Sair . . . ");
+            Console.ReadLine();
+        }
+    }
+}
