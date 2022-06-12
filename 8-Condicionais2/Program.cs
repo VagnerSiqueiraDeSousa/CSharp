81f117a351eea449dc3ec96df72dd30882e76cc4
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 18:44:03 -0300

    Add files via upload
    condicionais 2

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 68;
+            bool ehIdoso = idade >= 65;
+
+            Console.WriteLine(ehIdoso);
+
+            /*  int idade = 16;
+              // int quantidadePessoa = 2;
+
+              //bool acompanhado = quantidadePessoa >= 2;
+              bool acompanhado = true;
+
+              if (idade >= 18 && acompanhado == true) 
+              {
+                  Console.WriteLine("Pode entrar! ");
+              }
+
+              else
+
+              {
+                  Console.WriteLine("Não pode entrar");
+              }
+
+              */
+
+            Console.ReadLine();
+        }
+    }
+}
